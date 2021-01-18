0ed37334bb430db403fcfb9dfe2888a8d581cd4e
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2021-01-18 16:50:16 +0000

    punto 2

--- ProductApp/src/app/app-routing.module.ts
+++ ProductApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search', component:},
+  {path:'about', component:AboutComponent},
+  {path:'product/:id', component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
