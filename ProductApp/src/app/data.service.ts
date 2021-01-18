97fd4b5249fd760cb9f9c6acb123151b11df4ead
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2021-01-18 17:15:05 +0000

    punto 4

--- ProductApp/src/app/data.service.ts
+++ ProductApp/src/app/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  searchprod(query:string){
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=2&json=true`;
+    let obsprod = this.http.get(url);
+    return obsprod;
+  }
 }
--- ProductApp/src/app/search/search.component.ts
+++ ProductApp/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -6,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  query:string;
+  obsprod:Observable<Object>;
+  results:any;
+  constructor(public products: DataService) { }
+  submit(query:HTMLInputElement): void{
+    if(!query.value){
+      return;
+    }
+    this.query = query.value;
+    this.obsprod = this.products.searchprod(this.query);
+    this.obsprod.subscribe((data) => this.results = data);
+  }
 
   ngOnInit(): void {
   }
